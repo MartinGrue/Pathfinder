c0bd11ee93c9c48052f4aa2d1c82825173451caa
MartinGrue <gruebler.martin@outlook.de>
2020-05-01 22:49:52 +0200

    Animated ScollView v2.0

--- src/screen/Paths/FindYourPath.tsx
+++ src/screen/Paths/FindYourPath.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, {
+  useState,
+
+} from "react";
 import { Text, SafeAreaView, StyleSheet, View } from "react-native";
 
 import Animated from "react-native-reanimated";
 import { useMemoOne } from "use-memo-one";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 const {
   useCode,
   block,
@@ -15,18 +19,20 @@ const {
   eq,
   ceil,
   onChange,
-
+  interpolate,
+  Extrapolate,
   greaterThan,
   lessThan,
+  lessOrEq,
+  greaterOrEq,
+  multiply,
+  floor,
+  abs,
 } = Animated;
-
-interface FloaterProps {
+interface TextWrapperProps {
   scrolly: Animated.Value<number>;
-  offset: number;
 }
-const Floater = ({ scrolly, offset }: FloaterProps) => {
-  const [index, setindex] = useState(offset);
-  const TEXT_HEIGHT = 40;
+const TextWrapper = ({ scrolly }: TextWrapperProps) => {
   const WordArr: string[] = [
     "Erster",
     "Zweiter",
@@ -34,81 +40,161 @@ const Floater = ({ scrolly, offset }: FloaterProps) => {
     "Vierter",
     "Fuenfter",
     "Sechster",
+    "Siebter",
+    "Achter",
+    "Neunter",
+    "Zehnter",
   ];
 
-  const { lastScrolly } = useMemoOne(
+  const textHeight = 20;
+  const xOffset = 20;
+  const yMax = (textHeight * WordArr.length) / 2;
+  const xMax = (xOffset * WordArr.length) / 2;
+
+  return (
+    <Animated.View>
+      {WordArr.map((word, index) => {
+        return (
+          <LittleFloater
+            key={index}
+            {...{
+              scrolly,
+              xOffset,
+              textHeight,
+              yMax,
+              xMax,
+              word,
+              index,
+            }}
+          ></LittleFloater>
+        );
+      })}
+    </Animated.View>
+  );
+};
+interface LittleFloaterProps {
+  scrolly: Animated.Value<number>;
+  index: number;
+  word: string;
+  xOffset: number;
+  textHeight: number;
+  yMax: number;
+  xMax: number;
+}
+const LittleFloater = ({
+  scrolly,
+  index,
+  word,
+  xOffset,
+  textHeight,
+  yMax,
+  xMax,
+}: LittleFloaterProps) => {
+  const { outPutFrom, outPutTo, inputTo, prefScroll } = useMemoOne(
     () => ({
-      lastScrolly: new Animated.Value(0),
+      outPutFrom: new Animated.Value(0),
+      outPutTo: new Animated.Value(100),
+      inputTo: new Animated.Value(0.00001),
+      prefScroll: new Animated.Value(0),
     }),
     []
   );
+  const translateX = interpolate(scrolly, {
+    inputRange: [0, inputTo],
+    outputRange: [outPutFrom, outPutTo],
+    extrapolate: Extrapolate.CLAMP,
+  });
 
-  const translateX = offset * 10;
+  const translateY = interpolate(scrolly, {
+    inputRange: [0, 444],
+    outputRange: [0, -444],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  // const currentHeight = index * textHeight;
+  const currentHeight = ceil(sub(index * textHeight, floor(abs(scrolly))));
+  const xDiff = Math.abs(xMax - index * xOffset);
 
   useCode(
     () =>
       block([
-        onChange(scrolly, [
-          cond(greaterThan(ceil(scrolly), add(lastScrolly, 10)), [
-            set(lastScrolly, scrolly),
-            call([scrolly], () => {
-              setindex((prev) => prev + 1);
-            }),
-          ]),
-          cond(lessThan(ceil(scrolly), sub(lastScrolly, 10)), [
-            set(lastScrolly, scrolly),
-            call([scrolly], () => {
-              setindex((prev) => prev - 1);
-            }),
-          ]),
-          cond(eq(ceil(scrolly), 0), [
-            set(lastScrolly, scrolly),
-            call([scrolly], () => {
-              setindex(index);
-            }),
-          ]),
-        ]),
+        cond(
+          lessOrEq(abs(sub(prefScroll, scrolly)), 50),
+          [
+            cond(
+              greaterOrEq(currentHeight, yMax),
+              [
+                set(outPutFrom, xMax - xDiff),
+                set(outPutTo, xMax),
+                set(inputTo, xDiff + 0.00001),
+              ],
+              [
+                set(outPutFrom, index * xOffset),
+                set(outPutTo, 0),
+                set(inputTo, index * xOffset + 0.00001),
+              ]
+            ),
+          ],
+          [
+            cond(
+              greaterOrEq(index * textHeight, yMax),
+              [
+                set(outPutFrom, xMax - xDiff),
+                set(outPutTo, xMax),
+                set(inputTo, xDiff + 0.00001),
+              ],
+              [
+                set(outPutFrom, index * xOffset),
+                set(outPutTo, 0),
+                set(inputTo, index * xOffset + 0.00001),
+              ]
+            ),
+          ]
+        ),
+        [set(prefScroll, scrolly)],
       ]),
-    []
+    [scrolly]
   );
-  useCode(() => block([]), []);
   return (
-    <View
+    <Animated.View
       style={{
-        // position: "absolute",
-        translateX: translateX,
+        translateY,
+        translateX,
         backgroundColor: "orange",
       }}
     >
       <View>
-        <Text style={{ textAlign: "center", fontSize: 40 }}>
-          {WordArr[index]}
-        </Text>
+        <TouchableWithoutFeedback>
+          <Text style={{ textAlign: "center", height: textHeight }}>
+            {word}
+          </Text>
+        </TouchableWithoutFeedback>
       </View>
-    </View>
+    </Animated.View>
   );
 };
+interface FloaterProps {
+  scrolly: Animated.Value<number>;
+  offset: number;
+}
+
 
 export default () => {
   const { scrolly } = useMemoOne(
     () => ({
       scrolly: new Animated.Value(0),
-      outPutFrom: new Animated.Value(0),
-      outPutTo: new Animated.Value(100),
     }),
     []
   );
 
   // const scrolly = new Animated.Value(0);
   useCode(() => block([call([scrolly], (s) => console.log(s))]), []);
-  const arr_names = new Array<number>(10);
-  for (var i = 0; i < arr_names.length; i++) {
-    arr_names[i] = i;
-  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Animated.ScrollView
         scrollEventThrottle={1}
+        overScrollMode="never"
+        decelerationRate={0.9}
         onScroll={event([
           {
             nativeEvent: {
@@ -123,13 +209,8 @@ export default () => {
         {/* <Textes num={100} {...{ scrolly }}></Textes> */}
       </Animated.ScrollView>
       <View style={{ position: "absolute" }}>
-        <Floater {...{ scrolly }} offset={0}></Floater>
-        <Floater {...{ scrolly }} offset={1}></Floater>
-        <Floater {...{ scrolly }} offset={2}></Floater>
-        <Floater {...{ scrolly }} offset={3}></Floater>
-        <Floater {...{ scrolly }} offset={4}></Floater>
-        <Floater {...{ scrolly }} offset={5}></Floater>
-        <Floater {...{ scrolly }} offset={6}></Floater>
+        <TextWrapper {...{ scrolly }}></TextWrapper>
+
       </View>
     </SafeAreaView>
   );
