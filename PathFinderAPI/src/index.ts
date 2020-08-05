4d5ef4bf2a10c8277bc251af22186ccb92addcb5
MartinGrue <gruebler.martin@outlook.de>
2020-08-05 19:54:56 +0200

    added more assets

--- PathFinderAPI/src/index.ts
+++ PathFinderAPI/src/index.ts
@@ -1,42 +1,40 @@
+const mongoose = require("mongoose");
+import express from "express";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/authRoutes";
+import trackRoutes from "./routes/trackRoutes";
 
-const mongoose = require('mongoose');import express from "express";
-import bodyParser from 'body-parser'
-import authRoutes from './routes/authRoutes'
-import trackRoutes from './routes/trackRoutes'
-
-import { IUser } from './models/User';
+import { IUser } from "./models/User";
 const app = express();
-declare module 'express-serve-static-core' {
+declare module "express-serve-static-core" {
   interface Request {
-    user: IUser | null 
+    user: IUser | null;
   }
 }
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
-
 
 const mongoConnectionString =
   "mongodb+srv://root:root@cluster0-lgncb.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(mongoConnectionString, {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongo instance");
 });
-mongoose.connection.on("error", (error:any) => {
+mongoose.connection.on("error", (error: any) => {
   console.error(error);
 });
-app.get('/', (req, res) => {
-  res.send(`Your email:`);
-});
-app.get('/bli', (req, res) => {
-  res.send(`Your email:`);
-});
-
-app.listen(port, () => {
-  console.log("Im listening");
-});
+// app.get('/', (req, res) => {
+//   res.send(`Your email:`);
+// });
+// app.get('/bli', (req, res) => {
+//   res.send(`Your email:`);
+// });
 app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(trackRoutes);
+app.listen(port, () => {
+  console.log("Im listening");
+});
